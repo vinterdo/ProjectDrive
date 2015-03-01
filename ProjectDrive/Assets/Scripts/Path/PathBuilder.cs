c9ea26f975162b432e2ebe74f61ba3f813acfcc5
KebabuTurka <iron.dantix@gmail.com>
2015-03-01 19:09:58 +0100

    Almost... Need to analyze strength variables, and do something with this shit...

--- ProjectDrive/Assets/Scripts/Path/PathBuilder.cs
+++ ProjectDrive/Assets/Scripts/Path/PathBuilder.cs
@@ -8,7 +8,11 @@ public class PathBuilder : MonoBehaviour
     MeshFilter filter;
 
     public float width = 2;
+    public float distance = 5;
+    public float Xstr = 3;
+    public float Zstr = 1.2f;
     public float offroad = 0.1f;
+    public float offUV = 0.2f;
     public float step = 0.01f;
 
 	// Use this for initialization
@@ -21,28 +25,44 @@ public class PathBuilder : MonoBehaviour
         m.Clear();
 
         List<Vector3> verts = new List<Vector3>();
+        List<Vector2> uvs = new List<Vector2>();
         List<int> tris = new List<int>();
 
         for(int i = 0;i<1/step;i++)
         {
             Vector3 v = spline.GetPoint(i * step);
-            Vector3 next = spline.GetPoint((i + 1) * step);
+            Vector3 next;
+            if(i<1)
+                next = spline.GetPoint((i + 1) * step);
+            else
+                next = v;
 
             float angle = Vector3.Angle(v, next);
 
-            Vector3 temp = new Vector3(v.x-width/2,v.y,v.z);
+            Vector3 temp;
+            float x,y,z;
+
+            if (i == 0)
+                y = 0;
+            else
+                y = i * distance;
+
+            x = v.x * Xstr;
+            z = v.z * Zstr;
+            
+            temp = new Vector3(x - width / 2, y, z);
             temp = RotatePointAroundPivot(temp, v, Quaternion.Euler(0, angle, 0));
             verts.Add(temp);
 
-            temp = new Vector3(v.x - (width / 2) + offroad, v.y, v.z);
+            temp = new Vector3(x - (width / 2) + offroad, y, z);
             temp = RotatePointAroundPivot(temp, v, Quaternion.Euler(0, angle, 0));
             verts.Add(temp);
 
-            temp = new Vector3(v.x + (width / 2) - offroad, v.y, v.z);
+            temp = new Vector3(x + (width / 2) - offroad, y, z);
             temp = RotatePointAroundPivot(temp, v, Quaternion.Euler(0, angle, 0));
             verts.Add(temp);
 
-            temp = new Vector3(v.x + width / 2, v.y, v.z);
+            temp = new Vector3(x + width / 2, y, z);
             temp = RotatePointAroundPivot(temp, v, Quaternion.Euler(0, angle, 0));
             verts.Add(temp);
         }
