73e7777fd82d6820442e2f0fe9c2133edab0bdce
KebabuTurka <iron.dantix@gmail.com>
2015-03-18 19:53:32 +0100

    Added old vehicle stuff.

---
+++ ProjectDrive/Assets/Scripts/VehiclePlayerController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VehiclePlayerController : MonoBehaviour 
+{
+    public GameObject VehicleObject;
+
+	void Update () 
+    {
+        Vehicle v = VehicleObject.GetComponent<Vehicle>();
+
+        v.ControllForward = Input.GetAxis("Vertical");
+        v.ControllSides = Input.GetAxis("Horizontal");
+	}
+}
