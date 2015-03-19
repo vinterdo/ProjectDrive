7f1a902ce0537c8ee8c121e4a3fe7303a8ecce89
Vinterdo <pio4426@gmail.com>
2015-03-19 19:36:39 +0100

    Script for Race update

--- ProjectDrive/Assets/Scripts/RaceManager.cs
+++ ProjectDrive/Assets/Scripts/RaceManager.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System;
 using System.Collections.Generic;
 
 public class RaceManager : MonoBehaviour 
@@ -21,9 +22,9 @@ public class RaceManager : MonoBehaviour
     {
         for(int i=0; i < Vehicles.Count; i++)
         {
-            if(Vector3.Magnitude(Vehicles[i].transform.position, GetNextCheckpoint(Checkpoints[Vehicles[i])) < MaxDistFromCheckpoint)
+            if(Vector3.Magnitude(Vehicles[i].transform.position - GetNextCheckpoint(Checkpoints[Vehicles[i]]).transform.position) < MaxDistFromCheckpoint)
 			{
-				Checkpoints[Vehicles[i]] = GetNextCheckpoint(Checkpoints[Vehicles[i]);
+				Checkpoints[Vehicles[i]] = GetNextCheckpoint(Checkpoints[Vehicles[i]]);
 				Debug.Log("switched checpoint");
 			}
         }
@@ -31,14 +32,14 @@ public class RaceManager : MonoBehaviour
 
     public int GetVehiclePos(GameObject Vehicle)
     {
-
+        return 0;
     }
 	
 	public GameObject GetNextCheckpoint(GameObject Checkpoint)
 	{
-		int index = PathObject.GetComponentInChildren<Transform>.IndexOf(Checkpoint);
-		var List<Transform> path = PathObject.GetComponentsInChildren<Transform>();
-		return path[Mathf.Clamp(index + 1,0, path.Count)];
+		int index = Array.IndexOf(PathObject.GetComponentsInChildren<Transform>(), Checkpoint.transform);
+		Transform[] path = PathObject.GetComponentsInChildren<Transform>();
+		return path[Mathf.Clamp(index + 1,0, path.Length)].gameObject;
 	}
 	
     public void BeginRace()
