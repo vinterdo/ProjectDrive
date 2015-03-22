87aab54331ee05a81845364bbd97af1c9c8581cc
KebabuTurka <iron.dantix@gmail.com>
2015-03-22 16:32:46 +0100

    Narzędzia do edycji AIPath. Niechcący wyjebałem ścieżke jedną. Zmieniony lekko teren.

---
+++ ProjectDrive/Assets/Editor/AIPathEditor.cs
@@ -0,0 +1,89 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(AIPath))]
+public class AIPathEditor : Editor
+{
+    bool edit = false;
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        AIPath path = (AIPath)target;
+        if (GUILayout.Button("Snap down"))
+        {
+            path.SnapDown();
+        }
+
+        if (GUILayout.Button("Snap up"))
+        {
+            path.SnapUp();
+        }
+
+        if (GUILayout.Button("Up"))
+        {
+            path.Up();
+        }
+
+        if (GUILayout.Button("Down"))
+        {
+            path.Down();
+        }
+
+        if (GUILayout.Button("Moar"))
+        {
+            path.Moar();
+        }
+
+        if(edit)
+        {
+            GUI.color = Color.red;
+        }
+        else
+        {
+            GUI.color = Color.white;
+        }
+
+        if(GUILayout.Button("Edit Mode"))
+        {
+            edit = !edit;
+        }
+
+        if(Event.current.shift)
+        {
+            edit = !edit;
+        }
+    }
+
+    void OnSceneGUI()
+    {
+        AIPath path = (AIPath)target;
+        if (edit)
+        {
+            if (Event.current.type == EventType.MouseUp)
+            {
+                GUIUtility.hotControl = this.GetHashCode();
+                Ray worldRay = HandleUtility.GUIPointToWorldRay(Event.current.mousePosition);
+                RaycastHit hitInfo;
+                // Shoot this ray. check in a distance of 10000.
+                if (Physics.Raycast(worldRay, out hitInfo, 10000))
+                {
+                    if (path.empty)
+                    {
+                        GameObject g = (GameObject)Instantiate(path.empty, hitInfo.point, Quaternion.identity);
+                        g.name = "Waypoint" + path.count++;
+                        g.transform.parent = path.transform;
+                        EditorUtility.SetDirty(g);
+                    }
+                    else
+                    {
+                        Debug.LogError("There's no 'empty' object !");
+                    }
+                }
+                GUIUtility.hotControl = 0;
+            }
+            Event.current.Use();
+        }
+    }
+}
--- ProjectDrive/Assets/Scripts/AIPath.cs
+++ ProjectDrive/Assets/Scripts/AIPath.cs
@@ -4,12 +4,15 @@ using System.Collections.Generic;
 
 public class AIPath : MonoBehaviour 
 {
+    Transform[] Waypoints;
+    public GameObject empty;
+    public int count = 0;
 
     void OnDrawGizmos()
     {
         Gizmos.color = Color.white;
 
-        Transform[] Waypoints = GetComponentsInChildren<Transform>();
+        Waypoints = GetComponentsInChildren<Transform>();
 
         for (int i = 0; i < Waypoints.Length; i++)
         {
@@ -21,9 +24,89 @@ public class AIPath : MonoBehaviour
             Gizmos.DrawLine(Waypoints[i].position, Waypoints[i + 1].position);
         }
 
-        Gizmos.DrawLine(Waypoints[Waypoints.Length - 1].position, Waypoints[0].position);
+        Gizmos.DrawLine(Waypoints[Waypoints.Length - 1].position, Waypoints[0].position);        
+    }
 
-        
+    public void SnapDown()
+    {
+        RaycastHit c;
+
+        for (int i = 0; i < Waypoints.Length; i++)
+        {
+            if (Physics.Raycast(Waypoints[i].transform.position, -Vector3.up, out c))
+            {
+                Waypoints[i].transform.position = c.point + Vector3.up;
+            }
+        }
+    }
+
+    public void SnapUp()
+    {
+        RaycastHit c;
+
+        for (int i = 0; i < Waypoints.Length; i++)
+        {
+            if (Physics.Raycast(Waypoints[i].transform.position, Vector3.up, out c))
+            {
+                Waypoints[i].transform.position = c.point + Vector3.up;
+            }
+        }
+    }
+
+    public void Up()
+    {
+        for (int i = 0; i < Waypoints.Length; i++)
+        {
+            Waypoints[i].transform.position += Vector3.up;
+        }
+    }
+
+    public void Down()
+    {
+        for (int i = 0; i < Waypoints.Length; i++)
+        {
+            Waypoints[i].transform.position -= Vector3.up;
+        }
+    }
+
+    public void Moar()
+    {
+        if (empty)
+        {
+            Transform[] waypoints = GetComponentsInChildren<Transform>();
+            List<Vector3> newW = new List<Vector3>();
+
+            for (int i = 0; i < waypoints.Length - 1; i++)
+            {
+                newW.Add(waypoints[i].position);
+
+                Vector3 p = (waypoints[i].position + waypoints[i + 1].position) / 2;
+
+                newW.Add(p);
+            }
+
+            newW.Add((waypoints[waypoints.Length - 1].position + transform.position) / 2);
+
+            int children = transform.childCount;
+            for (int i = 0; i < children; i++)
+            {
+                DestroyImmediate(transform.GetChild(0).gameObject);
+            }
+
+            int x = 0;
+            foreach (Vector3 v in newW)
+            {
+                GameObject t = (GameObject)Instantiate(empty, v, Quaternion.identity);
+                t.name = "Waypoint" + x++;
+                t.transform.parent = transform;
+            }
+
+            count = newW.Count;
+        }
+        else
+        {
+            Debug.LogError("There's no 'empty' object !");
+        }
     }
 
 }
